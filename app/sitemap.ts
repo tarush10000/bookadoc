e33ca68900717205a5553f10dc14bec68676f57b
Tarush Agarwal <tarushagarwal2003@gmail.com>
2025-11-20 01:10:05 +0530

    sitemap minor

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://bookadoc.com',
+            url: 'https://bookadoc-xi.vercel.app/',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
         },
         {
-            url: 'https://bookadoc.com/demo',
+            url: 'https://bookadoc-xi.vercel.app//demo',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://bookadoc.com/pricing',
+            url: 'https://bookadoc-xi.vercel.app//pricing',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://bookadoc.com/features',
+            url: 'https://bookadoc-xi.vercel.app//features',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.7,
         },
         {
-            url: 'https://bookadoc.com/contact',
+            url: 'https://bookadoc-xi.vercel.app//contact',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.6,
